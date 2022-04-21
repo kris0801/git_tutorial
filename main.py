a3bdb0a9048f60d7b1fe3c3312d62770b56a403d
kris0801 <kristopherpaz@ufm.edu>
2022-04-20 18:51:23 -0600

    Update main.py

--- main.py
+++ main.py
@@ -4,3 +4,15 @@ n2 = int(input('ingrese un número: '))
 suma = n1 + n2 
 
 print (suma)
+
+n1 = int(input('ingrese un número: '))
+n2 = int(input('ingrese un número: '))
+resta = n1 - n2 
+
+print (resta)
+
+n1 = int(input('ingrese un número: '))
+n2 = int(input('ingrese un número: '))
+division = n1/n2 
+
+print (division)
